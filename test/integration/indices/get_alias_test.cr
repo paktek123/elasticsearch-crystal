9ed1359ac8878736c772272fd2bc64777ad0d981
paktek123 <paktek123@gmail.com>
2017-05-28 20:37:43 +0100

    Update get_alias_test.cr

---
+++ test/integration/indices/get_alias_test.cr
@@ -0,0 +1,24 @@
+require "../../spec_helper"
+
+module Elasticsearch
+  module Test
+    class IndicesGetAliasTest
+      include Spec
+
+      context "Indices: Get Alias: " do
+        subject = Elasticsearch::Test::Client.new({:host => "localhost", :port => 9250})
+
+        Spec.after_each do
+          subject.indices.delete({:index => "testind"})
+        end
+
+        it "get an alias" do
+          subject.indices.create({:index => "testind"})
+          subject.indices.put_alias({:index => "testind", :name => "testali"})
+          subject.indices.get_alias({:index => "testind", :name => "testali"}).as(JSON::Any)
+          subject.indices.delete_alias({:index => "testind", :name => "testali"}) 
+        end
+      end
+    end
+  end
+end
